37d8a06fe6b4b719ed743ab2b06a36286dd8332a
Ole <olakas99@wp.pl>
2021-04-29 12:15:00 +0200

    phaser start

---
+++ script.js
@@ -0,0 +1,24 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 500,
+    height: 500,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+}
+
+function create ()
+{
+}
+
+function update ()
+{
+}
