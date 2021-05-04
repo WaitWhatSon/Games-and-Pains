6103cb588c6d0f4a4351235a64e6850a900cebfa
Ole <olakas99@wp.pl>
2021-05-04 18:44:17 +0200

    dodane poruszające się platformy

--- script.js
+++ script.js
@@ -5,18 +5,16 @@ var controls;
 var canvas_W = 750;
 var canvas_H = 500;
 
-var world_W = canvas_W;
-var world_H = canvas_H * 10;
-
 var background;
 
-var score = 0;
+var height_score = 0;
+var platforms_number = 2;
 var scoreText;
 
 var player;
 var player2;
 var platforms;
-var bread;
+//var bread;
 
 var cursors;
 var cursors2;
@@ -54,7 +52,7 @@ function preload ()
     this.load.image('background', 'svg/background.svg');
     this.load.spritesheet('duck', 'svg/duck.svg' , { frameWidth: 50, frameHeight: 50 });
 
-    this.load.image('bread', 'svg/bread.svg');
+    //this.load.image('bread', 'svg/bread.svg');
     this.load.image('block', 'svg/block.svg');
     this.load.image('platform', 'svg/platform.svg');
 }
@@ -67,18 +65,17 @@ function create ()
     // BACKGROUND IMAGE
     background = this.add.image(canvas_W/2, canvas_H/2, 'background');
     // WORLD BUILDING
-    platforms = this.physics.add.staticGroup();
-    addFirstLevelPlatform()
-    platform = this.physics.add.sprite(canvas_W/2, 0, "platform");
-    platform.setCollideWorldBounds(false);
-    platform.body.setAllowGravity(false);
-    platform.setImmovable(true);
+    platforms = this.physics.add.group();
+    addFirstLevelPlatform(this)
+    addSecondLevelPlatform(this)
+    // next platforms create in update()
     // ------- PLAYERS --------
     // PLAYER 1
     player = this.physics.add.sprite(50, 50, 'duck');
     cursors = this.input.keyboard.createCursorKeys();
     player.setBounce(0.3);
-    player.setCollideWorldBounds(true);
+    player.setCollideWorldBounds(false);
+    player.body.setMass(10);
     // PLAYER 2
     player2 = this.physics.add.sprite(canvas_W-50, 50, 'duck');
     keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
@@ -86,42 +83,35 @@ function create ()
     keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     player2.setBounce(0.3);
-    player2.setCollideWorldBounds(true);
-    
-
-    
-    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-    
-    
-    
-    
-
+    player2.setCollideWorldBounds(false);
+    player2.body.setMass(10);
+    // ---------- ANIMATIONS ------------
+    // -------- ducks animation ---------
     this.anims.create({
         key: 'left',
         frames: this.anims.generateFrameNumbers('duck', { start: 0, end: 2 }),
         frameRate: 10,
         repeat: -1
     });
-
     this.anims.create({
         key: 'turn',
         frames: [ { key: 'duck', frame: 4 } ],
         frameRate: 20
     });
-
     this.anims.create({
         key: 'right',
         frames: this.anims.generateFrameNumbers('duck', { start: 3, end: 5 }),
         frameRate: 10,
         repeat: -1
     });
-
+    // SCORE TEXT
+    scoreText = this.add.text(16, 16, 'platforms number: 0', { fontSize: '16px', fill: '#000' });
 
 
     
 
-
-    bread = this.physics.add.group({
+    
+    /*bread = this.physics.add.group({
         key: 'bread',
         repeat: 11,
         setXY: { x: 12, y: 0, stepX: 70 }
@@ -131,38 +121,26 @@ function create ()
     
         child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
     
-    });
+    });*/
 
 
     // PHYSICS
-    this.physics.add.overlap(player, bread, collectBread, null, this);
-    this.physics.add.overlap(player2, bread, collectBread, null, this);
+    //this.physics.add.overlap(player, bread, collectBread, null, this);
+    //this.physics.add.overlap(player2, bread, collectBread, null, this);
+    //this.physics.add.collider(bread, platforms);
 
     this.physics.add.collider(player, platforms);
     this.physics.add.collider(player2, platforms);
-    
     this.physics.add.collider(player, player2);
-        
-    this.physics.add.collider(bread, platforms);
-
-
-    this.physics.add.collider(player, platform);
-    this.physics.add.collider(player2, platform);
-
-
-
-   
 
 }
 
 // ------------------------------------
 function update ()
 {
-    //this.cameras.main.y += 1;
-    //background.y -= 1;
-    platform.y += 1;
-
-    platforms.y -= 1;
+    height_score += 1
+    updatePlatformsPosition();
+    scoreText.setText('platforms number: ' + platforms_number);
     
     // PLAYER
     if (cursors.left.isDown)
@@ -217,52 +195,119 @@ function update ()
 
 
 
-    
-
+    if (height_score == 200)
+    {
+        height_score = 1;
+        platforms_number += 1;
+        addNextLevelPlatform(this, platforms_number % 3 == 0);
+        removePlatformsOverMap();
+    }
 }
 
 
 
 
 
-// ------------- FUNCTIONS ----------------
-function collectBread (player, bread)
+// ============== FUNCTIONS ===============
+/*function collectBread (player, bread)
 {
     bread.disableBody(true, true);
-}
+}*/
 
-
-
-// world building
-function addFirstLevelPlatform()
+// --------------- WORLD ------------------
+// world building - first platform
+function addFirstLevelPlatform(scene)
 {
-    for(let i = 1; i < canvas_W/50; i++)
+    let platform = new Platform(scene, canvas_W/2, canvas_H/2, 'block', false);
+    this.platforms.add(platform)
+    platform.setCollideWorldBounds(false);
+    platform.body.setAllowGravity(false);
+    platform.setImmovable(true);
+}
+// second platform
+function addSecondLevelPlatform(scene)
+{
+    let platform = new Platform(scene, canvas_W/2, 50, 'platform', false);
+    this.platforms.add(platform)
+    platform.setCollideWorldBounds(false);
+    platform.body.setAllowGravity(false);
+    platform.setImmovable(true);
+}
+// next platforms
+function addNextLevelPlatform(scene, moving)
+{
+    // randomize x position of platform
+    x_w = Math.floor(Math.random() * (canvas_W-250/2 - 250/2)) + 250/2;
+    let platform = new Platform(scene, x_w, 0, 'platform', moving);
+    this.platforms.add(platform)
+    platform.setCollideWorldBounds(false);
+    platform.body.setAllowGravity(false);
+    platform.setImmovable(true);    
+}
+// update all platforms
+function updatePlatformsPosition()
+{
+    platforms.children.each((child) => {
+        child.y += 1;
+        if(child.moving)
+        {
+            if(child.left)
+            {
+                if (child.x > 0)
+                {
+                    child.x -= 2;
+                }
+                else
+                {
+                    child.x += 2;
+                    child.left = false;
+                }
+            }
+            else
+            {
+                if (child.x < canvas_W)
+                {
+                    child.x += 2;
+                }
+                else
+                {
+                    child.x -= 2;
+                    child.left = true;
+                }
+            }
+            
+        }
+    });
+}
+// remove platforms over map
+function removePlatformsOverMap()
+{
+    platforms.children.iterate((child) => {
+        try{
+            if (child.y > canvas_H + 10)
+            {
+                platforms.remove(child, true);
+            }        
+        } // expected Uncaught Type Error
+        catch(error)
+        {
+            ; //ignore
+        }
+    });
+}
+// =========== CLASSES ==========
+class Platform extends Phaser.Physics.Arcade.Sprite {
+    
+    constructor(scene, x, y, sprite, _moving)
     {
-        platforms.create(50*i, canvas_H, 'block');
+        super(scene, x, y, sprite);
+        scene.add.existing(this)
+        scene.physics.add.existing(this)
+
+        this.moving = _moving;
+        this.right = true;
     }
 }
-
-function addLeftPlatform(level)
-{
-    for(let i = 1; i < 6; i++)
-    {
-        platforms.create(50*i, canvas_H-level, 'block');
-    }
-}
-
-function addRightPlatform(level)
-{
-    for(let i = 1; i < 6; i++)
-    {
-        platforms.create(canvas_W - 50*i, canvas_H-level, 'block');
-    }
-}
-
-function platformsUpdatePosition()
-{
-
-}
-
 
 // ------------------------------------------------------------------
 // TODO 
