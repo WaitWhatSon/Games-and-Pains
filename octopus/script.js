37cddbb86e39a4c15283b1151fcff7a5a3cc71c7
Ole <olakas99@wp.pl>
2022-07-31 20:14:55 +0200

    wut

--- octopus/script.js
+++ octopus/script.js
@@ -57,6 +57,28 @@ window.onload = function() {
         on_touch_move(e);
     }
 
+    // let touchHandler = function(event) {
+    //     let x = 0, y = 0;
+      
+    //     if (event.touches && event.touches[0]) {
+    //         x = event.touches[0].clientX;
+    //         y = event.touches[0].clientY;
+    //     } else if (event.originalEvent && event.originalEvent.changedTouches[0]) {
+    //         x = event.originalEvent.changedTouches[0].clientX;
+    //         y = event.originalEvent.changedTouches[0].clientY;
+    //     } else if (event.clientX && event.clientY) {
+    //         x = event.clientX;
+    //         y = event.clientY;
+    //     }
+      
+    //     console.log(x)
+    //     console.log(y)
+    // }
+
+    // window.addEventListener('touchstart', touchHandler, false);
+    // window.addEventListener('touchmove', touchHandler, false);
+    // window.addEventListener('touchend', touchHandler, false);
+
 }
 
 function on_move(e){
